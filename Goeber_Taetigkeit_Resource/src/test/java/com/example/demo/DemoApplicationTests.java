9b7ca47cd626294a9cb5bd9774ee9f9a70a1d60b
Lukas Göber <l.goeber@htlstp.at>
2020-06-30 12:29:02 +0200

    Tests applied in source Code

--- Goeber_Taetigkeit_Resource/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Goeber_Taetigkeit_Resource/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.net.URL;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    private URL base;
+
+    @BeforeEach
+    public void setUp()  throws Exception{
+        this.base = new URL("http://localhost:8080/");
+    }
+
+    private final TestRestTemplate template = new TestRestTemplate();
+
     @Test
-    void contextLoads() {
+    void getWorkingTimeEmployee() throws Exception {
+        ResponseEntity<Integer> response = template.getForEntity(base.toString() + "employees/totalworkingtime/SCMI", Integer.class);
+        assert(response.getBody().equals(570));
     }
 
 }
